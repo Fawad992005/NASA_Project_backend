8625c0803eee4a0dd0d8253d25f824dd3dfd5cfa
Fawad992005 <fawaadahmed514@gmail.com>
2025-01-28 02:29:46 +0500

    Added commit

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(
 );
 app.use(morgan("combined"));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use("/planets", planetsRouter);
 app.use("/launches", launhcesRouter);
 app.get("/*", (req, res) => {
